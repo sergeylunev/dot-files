c46b6f3efdacade15ac67682e22b2acf35765fb3
Sergey Lunev <sergey@lunyov.ru>
2020-11-19 02:59:16 +0300

    Small updates for install script

--- ubuntu-install/install.sh
+++ ubuntu-install/install.sh
@@ -29,9 +29,7 @@ sudo snap install phpstorm --classic
 sudo snap install --classic code
 
 # Install some needed software
-sudo apt install gnome-tweak-tools --yes
-sudo apt install pritunl-client-electron --yes
-sudo apt install vim --yes
+sudo apt install gnome-tweak-tools pritunl-client-electron vim tmux --yes
 
 # Installing JetBrainsMono fonts
 mkdir JetBrainsMono
@@ -57,15 +55,25 @@ ln -s $(realpath gitconfig) $(realpath ~)/.gitconfig
 
 # Installing php 7.4 and some other needed tools
 sudo apt install php7.4	 --yes
-sudo apt install php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-intl php7.4-json php7.4-mbstring php7.4-pgsql php7.4-xml --yes
+sudo apt install php7.4-bcmath php-mongodb php-redis php-soap php-zip php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-intl php7.4-json php7.4-mbstring php7.4-pgsql php7.4-xml --yes
+sudo apt install php-dev --yes
 sudo apt install php-pear --yes
 sudp apt install php-xdebug --yes
+
+# Copy php config here
+
+# Installing kafka
+sudo apt install librdkafka-dev
+sudo pecl install rdkafka
+sudo pecl install mongodb
+# Add rdkafka and mongo to php config
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
+chmod +x composer.phar
+sudo mv ./composer.phar /usr/local/bin/composer
 # TODO: add here copying php config file for installed php 
 
 # Installing pgsql
@@ -91,3 +99,5 @@ curl -s https://api.github.com/repos/docker/compose/releases/latest \
 chmod +x docker-compose-Linux-x86_64
 sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 
+# Set zsh as default shell
+chsh -s $(which zsh)
