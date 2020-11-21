838daa3adba0630da0c85e66af1c2baa5cbfb4d5
Sergey Lunev <sergey@lunyov.ru>
2020-11-21 11:29:35 +0300

    Install php7.4 from ppa

--- ubuntu-install/programms/php.sh
+++ ubuntu-install/programms/php.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:ondrej/php
+
 sudo apt install php7.4	 --yes
-sudo apt install php7.4-bcmath php-redis php-soap php-zip php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-intl php7.4-json php7.4-mbstring php7.4-pgsql php7.4-xml --yes
+sudo apt install php7.4-bcmath php-redis php-soap php7.4-zip php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-intl php7.4-json php7.4-mbstring php7.4-pgsql php7.4-xml --yes
 sudo apt install php-dev php-pear php-xdebug --yes
 
 # Copy php config here
