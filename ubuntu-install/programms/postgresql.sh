78c8728b4f4e4418bd20990ec366311a158445cd
Sergey Lunev <sergey@lunyov.ru>
2020-11-23 22:29:33 +0300

    Add redis installation

--- ubuntu-install/programms/postgresql.sh
+++ ubuntu-install/programms/postgresql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
 
 sudo apt install postgresql-9.6 --yes
--- ubuntu-install/programms/pritunl.sh
+++ ubuntu-install/programms/pritunl.sh
@@ -4,5 +4,6 @@ sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
 deb https://repo.pritunl.com/stable/apt focal main
 EOF
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt update
 
 sudo apt install pritunl-client-electron --yes
