c3832c3190877e89975e58146b99baebd57b7846
Sergey Lunev <sergey@lunyov.ru>
2023-06-12 23:32:21 +0300

    NV chad

---
+++ chadnv/custom/init.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+
+vim.g.mapleader = "\\"
+
+opt.backup = false
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.cursorline = true
+opt.number = true
+opt.numberwidth = 4
+opt.ruler = false
+opt.relativenumber = true
+opt.conceallevel = 0
+opt.fileencoding = "utf-8"
+opt.hlsearch = true
+opt.ignorecase = true
+opt.swapfile = false
+opt.writebackup = false
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
