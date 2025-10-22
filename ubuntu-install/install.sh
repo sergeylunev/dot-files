62437d3affbb2a06eedbaa4652cd1d4a17fce6f6
Sergey Lunev <sergey@lunyov.ru>
2025-10-23 00:08:01 +0300

    update

--- ubuntu-install/install.sh
+++ ubuntu-install/install.sh
@@ -31,6 +31,32 @@ sudo snap install telegram-desktop
 sudo snap install code --classic
 sudo snap install steam
 
+# Docker
+cd $(realpath ~)/Downloads
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+sudo gdebi --non-interactive docker-desktop-amd64.deb
+rm docker-desktop-amd64.deb
+# Enable to start on startup
+systemctl --user enable docker-desktop
+###
+
+# NeoVim install and configure
+sudo snap install nvim --classic
+git clone https://github.com/sergeylunev/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+###
+
+# GH - github cli tool
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+###
+
 # AppImageLauncher. Helps with installing and handling AppImages
 cd $(realpath ~)/Downloads
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v3.0.0-beta-2/appimagelauncher_3.0.0-beta-2-gha280.e110527_amd64.deb
@@ -62,7 +88,6 @@ ln -s $(realpath zshrc) $(realpath ~)/.zshrc
 ###
 
 # Git config
-rm $(realpath ~)/.gitconfig
 cd $(realpath ~)/Projects/dot-files/
 ln -s $(realpath gitconfig) $(realpath ~)/.gitconfig
 ###
@@ -72,4 +97,5 @@ dconf load /org/gnome/terminal/ < $(realpath ~)/Projects/dot-files/gnome-termina
 ###
 
 # Set zsh as default shell
+# Will take afect after restart
 chsh -s $(which zsh)
