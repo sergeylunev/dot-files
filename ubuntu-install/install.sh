3c55ba7e6c5e19c582d3e49eabcb884a1177948a
Sergey Lunev <sergey@lunyov.ru>
2025-10-23 00:33:29 +0300

    update

--- ubuntu-install/install.sh
+++ ubuntu-install/install.sh
@@ -33,7 +33,7 @@ sudo snap install steam
 
 # Docker
 cd $(realpath ~)/Downloads
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
 sudo gdebi --non-interactive docker-desktop-amd64.deb
 rm docker-desktop-amd64.deb
 # Enable to start on startup
