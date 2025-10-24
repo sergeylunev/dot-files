5b64b7a1ce02b7551404b21d4e75735d497208c6
Sergey Lunev <sergey@lunyov.ru>
2025-10-24 11:22:10 +0300

    Update

--- ubuntu-install/install.sh
+++ ubuntu-install/install.sh
@@ -32,6 +32,21 @@ sudo snap install code --classic
 sudo snap install steam
 
 # Docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 cd $(realpath ~)/Downloads
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
 sudo gdebi --non-interactive docker-desktop-amd64.deb
@@ -78,6 +93,12 @@ mv $(realpath JetBrainsMono.zip) $(realpath ~)/.fonts
 cd $(realpath ~)/.fonts
 unzip JetBrainsMono.zip
 fc-cache -fv
+###
+
+# Install golang latest version
+goversion=$(curl https://go.dev/dl/\?mode\=json | grep -o 'go.*.linux-amd64.tar.gz' | head -n 1 | tr -d '\r\n')
+wget "https://dl.google.com/go/$goversion"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $goversion
 ###
 
 # ZSH and OhMyZSH
